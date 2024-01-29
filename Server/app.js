a50c3c95924c5f16a73d8fcd9d608866545f9f02
Aaron Zheng <aarondzheng1@gmail.com>
2024-01-29 17:39:13 +0800

    Adding server code. Added rooms property.

--- Server/app.js
+++ Server/app.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const http = require('http');  // Use the 'https' module instead of 'http'
 const crypto = require('crypto');
 const WebSocket = require('ws');
+const http = require('http');  // Use the 'https' module instead of 'http' for production
+
 const modifyRoom = require('./websocket-room');
 const {rooms, maxNumberOfPlayers} = require('./global-properties');
 
 const app = express();
 const port = 3000;
 
-// Create an HTTPS server
 const server = http.createServer(app);
+
+// For a live server
+// // Load your SSL/TLS certificate and private key
+// const serverOptions = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/impactsservers.com/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/impactsservers.com/fullchain.pem'),
+// };
+
+// // Create an HTTPS server
+// const server = https.createServer(serverOptions, app);
+
+// SECTION: UPGRADING HTTP TO WEBSOCKETS
+// -----------------------
 
 // Upgrade the HTTP server to a WebSocket server on '/ws' route
 server.on('upgrade', (request, socket, head) => {
@@ -20,7 +33,7 @@ server.on('upgrade', (request, socket, head) => {
     let wss 
 
     // If room is empty or all rooms are currently full, create a new room
-    if (rooms.length == 0 || roomIsNotAvaliable()) {
+    if (rooms.length == 0 || roomIsNotAvailable()) {
       const roomUUID = crypto.randomUUID()
       wss = new WebSocket.Server({ noServer: true });
 
@@ -53,6 +66,7 @@ server.on('upgrade', (request, socket, head) => {
   }
 });
 
+// Algorithm to put the user in an available room with the most people
 function returnMostSuitableRoomAndUpdateRoomProperty() {
   let numberOfPlayersInPreviousRoom = 0;
   let returnRoom;
@@ -67,16 +81,17 @@ function returnMostSuitableRoomAndUpdateRoomProperty() {
   return returnRoom.websocketServer;
 }
 
-function roomIsNotAvaliable() {
-  let noRoomsAvaliable = true;
+// Helper function to determine if any rooms are available to join
+function roomIsNotAvailable() {
+  let noRoomsAvailable = true;
   for (const room of rooms) {
     if (room.numberOfPlayers > 0 && room.numberOfPlayers < maxNumberOfPlayers) {
-      noRoomsAvaliable = false;
+      noRoomsAvailable = false;
       // Exit the loop as soon as an available room is found
       break;  
     }
   }
-  return noRoomsAvaliable;
+  return noRoomsAvailable;
 };
 
 server.listen(port, () => {
